4f2f9d00eaad608ff99235420f486470a8983847
Dries Renmans <dries.renmans@telenet.be>
2017-03-30 20:51:01 +0200

    Set tabs to 4 spaces instead of 2.

--- breadProofer/breadProofer.ino
+++ breadProofer/breadProofer.ino
@@ -13,54 +13,54 @@ dht11 DHT11_3;
 
 void setup()
 {
-    pinMode(5, OUTPUT);     //Set all relay pins as outputs
+    pinMode(5, OUTPUT); //Set all relay pins as outputs
     pinMode(6, OUTPUT);
     pinMode(7, OUTPUT);
 }
 
 void loop()
 {
-  float sumtemp = 0;    //Reset sumtemp at the start of each for loop
-  float sumhum = 0;     //Reset sumhum at the start of each for loop
-  for (int i = 0; i < 10; i++)
-  {
-    DHT11_1.read(DHT11_1_PIN);    //Read sensor 1
-    float temp1 = DHT11_1.temperature;
-    float hum1 = DHT11_1.humidity;
+    float sumtemp = 0;    //Reset sumtemp at the start of each for loop
+    float sumhum = 0;     //Reset sumhum at the start of each for loop
+    for (int i = 0; i < 10; i++)    //Measure temperature and humidity 10 times for each sensor
+    {
+        DHT11_1.read(DHT11_1_PIN);    //Read sensor 1
+        float temp1 = DHT11_1.temperature;
+        float hum1 = DHT11_1.humidity;
 
-    DHT11_2.read(DHT11_2_PIN);    //Read sensor 2
-    float temp2 = DHT11_2.temperature;
-    float hum2 = DHT11_2.humidity;
+        DHT11_2.read(DHT11_2_PIN);    //Read sensor 2
+        float temp2 = DHT11_2.temperature;
+        float hum2 = DHT11_2.humidity;
 
-    DHT11_3.read(DHT11_3_PIN);    //Read sensor 3
-    float temp3 = DHT11_3.temperature;
-    float hum3 = DHT11_3.humidity;
+        DHT11_3.read(DHT11_3_PIN);    //Read sensor 3
+        float temp3 = DHT11_3.temperature;
+        float hum3 = DHT11_3.humidity;
 
-    sumtemp += temp1 += temp2 += temp3;
-    sumhum += hum1 += hum2 += hum3;
+        sumtemp += temp1 += temp2 += temp3;
+        sumhum += hum1 += hum2 += hum3;
 
-    delay(1000);    //Length of the measuring interval / 10
-  }
+        delay(1000);    //Length of the measuring interval / 10
+    }
 
-  float avgtemp = sumtemp / 30;   //Calculate the average temperature over the measuring interval (# of sensors * # of measurements)
-  float avghum = sumhum / 30;   //Calculate the average humidity over the measuring interval (# of sensors * # of measurements)
+    float avgtemp = sumtemp / 30;   //Calculate the average temperature over the measuring interval (# of sensors * # of measurements)
+    float avghum = sumhum / 30;   //Calculate the average humidity over the measuring interval (# of sensors * # of measurements)
 
-  if (avgtemp < 35) //Low temperature at which the heating elements turn on
-  {
-      digitalWrite(RELAY_TEMP_1_PIN, HIGH);
-      digitalWrite(RELAY_TEMP_2_PIN, HIGH);
-  }
-  else if (avg temp > 38)   //High temperature at which the heating elements turn off
-  {
-      digitalWrite(RELAY_TEMP_1_PIN, LOW);
-      digitalWrite(RELAY_TEMP_2_PIN, LOW);
-  }
-  if (avghum < 60)  //Low humidity at which the humidifier turns on
-  {
-      digitalWrite(RELAY_HUM_PIN, HIGH);
-  }
-  else if (avghum > 80) //High humidity at which the humidifier turns off
-  {
-      digitalWrite(RELAY_HUM_PIN, LOW);
-  }
+    if (avgtemp < 35) //Low temperature at which the heating elements turn on
+    {
+        digitalWrite(RELAY_TEMP_1_PIN, HIGH);
+        digitalWrite(RELAY_TEMP_2_PIN, HIGH);
+    }
+    else if (avg temp > 38)   //High temperature at which the heating elements turn off
+    {
+        digitalWrite(RELAY_TEMP_1_PIN, LOW);
+        digitalWrite(RELAY_TEMP_2_PIN, LOW);
+    }
+    if (avghum < 60)  //Low humidity at which the humidifier turns on
+    {
+        digitalWrite(RELAY_HUM_PIN, HIGH);
+    }
+    else if (avghum > 80) //High humidity at which the humidifier turns off
+    {
+        digitalWrite(RELAY_HUM_PIN, LOW);
+    }
 }
