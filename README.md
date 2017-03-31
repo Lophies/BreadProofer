511026ec01f534535a36bf2b5fa785e2934517a5
Dries Renmans <dries.renmans@telenet.be>
2017-03-31 16:02:34 +0200

    Edited comments

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BreadProofer
 A program to control the temperature and humidity inside a bread proofing box using 3 DHT11 sensors and relays to drive a humidifier and 2 heating elements.
+
+Hardware used:
+
+- Arduino-compatible board (1x)
+- SODIAL 5V 4-Channel Relay Module (1x)
+- Ecloud Shop DHT11 sensor (3x)
+- Heating elements (to be determined)
--- breadProofer/breadProofer.ino
+++ breadProofer/breadProofer.ino
@@ -39,27 +39,28 @@ void loop()
         sumtemp += temp1 += temp2 += temp3;
         sumhum += hum1 += hum2 += hum3;
 
-        delay(1000);    //Length of the measuring interval / 10
+        delay(1000);    //Length of the measuring interval / # of measurements
+                        //Note: DHT11 has a 1 Hz sampling rate so the delay must be >= 1000 ms
     }
 
     float avgtemp = sumtemp / 30;   //Calculate the average temperature over the measuring interval (# of sensors * # of measurements)
     float avghum = sumhum / 30;   //Calculate the average humidity over the measuring interval (# of sensors * # of measurements)
 
-    if (avgtemp < 35) //Low temperature at which the heating elements turn on
+    if (avgtemp < 35) //Low temperature at which the heating elements turn on (in °C)
     {
         digitalWrite(RELAY_TEMP_1_PIN, HIGH);
         digitalWrite(RELAY_TEMP_2_PIN, HIGH);
     }
-    else if (avg temp > 38)   //High temperature at which the heating elements turn off
+    else if (avg temp > 38)   //High temperature at which the heating elements turn off (in °C)
     {
         digitalWrite(RELAY_TEMP_1_PIN, LOW);
         digitalWrite(RELAY_TEMP_2_PIN, LOW);
     }
-    if (avghum < 60)  //Low humidity at which the humidifier turns on
+    if (avghum < 60)  //Low humidity at which the humidifier turns on (in %)
     {
         digitalWrite(RELAY_HUM_PIN, HIGH);
     }
-    else if (avghum > 80) //High humidity at which the humidifier turns off
+    else if (avghum > 80) //High humidity at which the humidifier turns off (in %)
     {
         digitalWrite(RELAY_HUM_PIN, LOW);
     }
